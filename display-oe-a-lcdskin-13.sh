edbb13b8e44f1bb5e71e974d41a0db3e16a3b97d
emil-nabil333 <elbrinselomda@yahoo.com>
2025-03-06 21:20:52 +0200

    Add files via upload

---
+++ display-oe-a-lcdskin-13.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+PLUGIN_NAME="enigma2-plugin-display-oe-a-lcdskin-13"
+PLUGIN_URL="https://github.com/emilnabil/download-plugins/raw/refs/heads/main/Display-Skin/${PLUGIN_NAME}.ipk"
+PLUGIN_FILE="/tmp/${PLUGIN_NAME}.ipk"
+
+sleep 1
+echo "Downloading new version of $PLUGIN_NAME..."
+cd /tmp
+
+if ! curl -k -L -o "$PLUGIN_FILE" "$PLUGIN_URL"; then
+    echo "Curl failed, trying wget..."
+    if ! wget -O "$PLUGIN_FILE" "$PLUGIN_URL"; then
+        echo "Download failed!"
+        exit 1
+    fi
+fi
+
+echo "Installing $PLUGIN_NAME..."
+opkg install --force-reinstall "$PLUGIN_FILE"
+
+if [ $? -eq 0 ]; then
+    echo "Installation completed successfully!"
+else
+    echo "Installation failed!"
+    exit 1
+fi
+
+rm -f "$PLUGIN_FILE"
+
+echo "Done!"
+exit 0
+
+
+
+
+
+
+
+
