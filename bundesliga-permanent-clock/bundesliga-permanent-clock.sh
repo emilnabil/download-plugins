accd2c6d37b305cbebd2e892ce214188db2c0b02
emil-nabil333 <elbrinselomda@yahoo.com>
2025-03-14 19:04:23 +0200

    Add files via upload

--- bundesliga-permanent-clock/bundesliga-permanent-clock.sh
+++ bundesliga-permanent-clock/bundesliga-permanent-clock.sh
@@ -1,44 +1,48 @@
 #!/bin/sh
-
+## Command=wget https://raw.githubusercontent.com/emilnabil/download-plugins/refs/heads/main/bundesliga-permanent-clock/bundesliga-permanent-clock.sh -O - | /bin/sh
+##
 PLUGIN_DIR="/usr/lib/enigma2/python/Plugins/Extensions/BundesligaPermanentClock"
 STATUS_FILE="/var/lib/opkg/status"
 PACKAGE_NAME="enigma2-plugin-extensions-bundesligapermanentclock"
+TMP_DIR="/var/volatile/tmp"
+PLUGIN="bundesliga-permanent-clock"
+URL="https://github.com/emilnabil/download-plugins/raw/refs/heads/main/bundesliga-permanent-clock/bundesliga-permanent-clock.tar.gz"
+PACKAGE_PATH="$TMP_DIR/${PLUGIN}.tar.gz"
 
 if [ -d "$PLUGIN_DIR" ]; then
     echo "> Removing package, please wait..."
-    rm -rf "$PLUGIN_DIR" > /dev/null 2>&1
+    sleep 1
+    rm -rf "$PLUGIN_DIR"
 
-    if grep -q "$PACKAGE_NAME" "$STATUS_FILE"; then
-        opkg remove "$PACKAGE_NAME" > /dev/null 2>&1
+    if opkg list-installed | grep -q "^$PACKAGE_NAME "; then
+        opkg remove "$PACKAGE_NAME" >/dev/null 2>&1
     fi
 
     echo "*******************************************"
     echo "*             Removal Finished            *"
     echo "*******************************************"
+    sleep 1
 fi
 
-# Plugin details
-PLUGIN="bundesliga-permanent-clock"
-VERSION="1.0"
-URL="https://gitlab.com/eliesat/extensions/-/raw/main/bundesliga-permanent-clock/bundesliga-permanent-clock-1.0.tar.gz"
-PACKAGE_PATH="/var/volatile/tmp/${PLUGIN}.tar.gz"
-
-# Download & install
 echo "> Downloading ${PLUGIN} package, please wait..."
 sleep 2
 
-if wget -O "$PACKAGE_PATH" --no-check-certificate "$URL"; then
+if wget -q -O "$PACKAGE_PATH" --no-check-certificate "$URL"; then
+    echo "> Extracting package..."
     if tar -xzf "$PACKAGE_PATH" -C /; then
         echo "> ${PLUGIN} package installed successfully"
     else
-        echo "> ${PLUGIN} package installation failed"
-        sleep 2
+        echo "> Package extraction failed"
+        rm -f "$PACKAGE_PATH"
+        exit 1
     fi
-    rm -rf "$PACKAGE_PATH" >/dev/null 2>&1
+    rm -f "$PACKAGE_PATH"
 else
-    echo "> Failed to download ${PLUGIN}-${VERSION} package"
+    echo "> Failed to download ${PLUGIN} package"
+    exit 1
 fi
 
 exit 0
+
 
 
