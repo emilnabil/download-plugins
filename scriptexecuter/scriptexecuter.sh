6aaef46fe8b4ab2e3b451ad3d056dca72f8e6e1b
emil-nabil333 <elbrinselomda@yahoo.com>
2025-03-15 10:09:49 +0200

    Add files via upload

--- scriptexecuter/scriptexecuter.sh
+++ scriptexecuter/scriptexecuter.sh
@@ -1,42 +1,42 @@
 #!/bin/sh
+## Command: wget https://raw.githubusercontent.com/emilnabil/download-plugins/refs/heads/main/scriptexecuter/scriptexecuter.sh -O - | /bin/sh
 
-if [ -d /usr/lib/enigma2/python/Plugins/Extensions/ScriptExecuter ]; then
-echo "> removing package please wait..."
-sleep 3s 
-rm -rf /usr/lib/enigma2/python/Plugins/Extensions/ScriptExecuter > /dev/null 2>&1
+PLUGIN_PATH="/usr/lib/enigma2/python/Plugins/Extensions/ScriptExecuter"
+PACKAGE_NAME="enigma2-plugin-extensions-scriptexecuter"
+STATUS_FILE="/var/lib/opkg/status"
 
-status='/var/lib/opkg/status'
-package='enigma2-plugin-extensions-scriptexecuter'
-
-if grep -q $package $status; then
-opkg remove $package > /dev/null 2>&1
+if [ -d "$PLUGIN_PATH" ]; then
+    echo "> Removing package, please wait..."
+    sleep 3
+    rm -rf "$PLUGIN_PATH" >/dev/null 2>&1
+    if grep -q "$PACKAGE_NAME" "$STATUS_FILE"; then
+        opkg remove "$PACKAGE_NAME" >/dev/null 2>&1
+    fi
+    echo "*******************************************"
+    echo "*             Removal Finished            *"
+    echo "*******************************************"
+    sleep 1
 fi
 
-echo "*******************************************"
-echo "*             Removed Finished            *"
-echo "*******************************************"
+PLUGIN="scriptexecuter"
+VERSION="1.0"
+DOWNLOAD_URL="https://github.com/emilnabil/download-plugins/raw/refs/heads/main/scriptexecuter/scriptexecuter.tar.gz"
+TAR_GZ_FILE="/var/volatile/tmp/${PLUGIN}.tar.gz"
+
+echo "> Downloading $PLUGIN package, please wait..."
 sleep 1
-plugin=scriptexecuter
-version=1.0
-url=https://gitlab.com/eliesat/extensions/-/raw/main/scriptexecuter/scriptexecuter-1.0.tar.gz
-package=/var/volatile/tmp/$plugin-$version.tar.gz
+wget -O "$TAR_GZ_FILE" --no-check-certificate "$DOWNLOAD_URL"
+tar -xzf "$TAR_GZ_FILE" -C /
+EXTRACT_STATUS=$?
+rm -rf "$TAR_GZ_FILE" >/dev/null 2>&1
 
-#download & install
-echo "> Downloading $plugin-$version package  please wait ..."
-sleep 1
-
-wget -O $package --no-check-certificate $url
-tar -xzf $package -C /
-extract=$?
-rm -rf $package >/dev/null 2>&1
-
-echo ''
-if [ $extract -eq 0 ]; then
-echo "> $plugin-$version package installed successfully"
+echo ""
+if [ $EXTRACT_STATUS -eq 0 ]; then
+    echo "> Package installed successfully"
 else
-
-echo "> $plugin-$version package installation failed"
-sleep 2
+    echo "> $PLUGIN package installation failed"
+    sleep 2
 fi
-  fi
+
 exit 0
+
