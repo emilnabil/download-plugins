f985c34ff986ce14383a217e8746ec8c5f28ff81
emil-nabil333 <elbrinselomda@yahoo.com>
2025-04-19 11:46:22 +0200

    Add files via upload

--- SmartAddonspanel/smart-Panel.sh
+++ SmartAddonspanel/smart-Panel.sh
@@ -1,92 +1,91 @@
 #!/bin/bash
-## Setup command:
-## wget https://raw.githubusercontent.com/emilnabil/download-plugins/refs/heads/main/SmartAddonspanel/smart-Panel.sh -O - | /bin/sh
+##setup command=wget https://raw.githubusercontent.com/emilnabil/download-plugins/main/SmartAddonspanel/smart-Panel.sh -O - | /bin/sh
 
-######### Only These 2 lines to edit with new version ######
-version='4.0.2'
+########
 changelog='\nFix little bugs\nUpdated Picons List'
 
 TMPPATH="/tmp/SmartAddonspanel"
-PLUGIN_URL="https://raw.githubusercontent.com/emilnabil/download-plugins/refs/heads/main/SmartAddonspanel"
+PLUGIN_URL="https://raw.githubusercontent.com/emilnabil/download-plugins/main/SmartAddonspanel"  
 
-PLUGINPATH="/usr/lib/enigma2/python/Plugins/Extensions/SmartAddonspanel"
-[ -d /usr/lib64 ] && PLUGINPATH="/usr/lib64/enigma2/python/Plugins/Extensions/SmartAddonspanel"
+if [ ! -d /usr/lib64 ]; then
+    PLUGINPATH="/usr/lib/enigma2/python/Plugins/Extensions/SmartAddonspanel"
+else
+    PLUGINPATH="/usr/lib64/enigma2/python/Plugins/Extensions/SmartAddonspanel"
+fi
 
 if [ -f /var/lib/dpkg/status ]; then
     STATUS="/var/lib/dpkg/status"
     OSTYPE="DreamOs"
-    PKG_MGR="apt-get"
-    INSTALL_CMD="install -y"
 else
     STATUS="/var/lib/opkg/status"
     OSTYPE="Dream"
-    PKG_MGR="opkg"
-    INSTALL_CMD="install"
 fi
 
-PYTHON=""
-Packagesix=""
-Packagerequests=""
-PLUGIN_SUBDIR=""
-
-if command -v python3 >/dev/null 2>&1 && python3 --version 2>&1 | grep -q '^Python 3\.'; then
+if python --version 2>&1 | grep -q '^Python 3\.'; then
     echo "You have Python3 image"
     PYTHON="PY3"
     Packagesix="python3-six"
     Packagerequests="python3-requests"
-    PLUGIN_SUBDIR="Py3"
-elif command -v python >/dev/null 2>&1; then
-    if python --version 2>&1 | grep -q '^Python 3\.'; then
-        echo "You have Python3 image"
-        PYTHON="PY3"
-        Packagesix="python3-six"
-        Packagerequests="python3-requests"
-        PLUGIN_SUBDIR="Py3"
-    else
-        echo "You have Python2 image"
-        PYTHON="PY2"
-        Packagerequests="python-requests"
-        PLUGIN_SUBDIR="Py2"
-    fi
 else
-    echo "Python is not installed!"
-    exit 1
+    echo "You have Python2 image"
+    PYTHON="PY2"
+    Packagerequests="python-requests"
 fi
 
 if [ "$PYTHON" = "PY3" ] && ! grep -qs "Package: $Packagesix" "$STATUS"; then
-    echo "Installing $Packagesix..."
-    $PKG_MGR update && $PKG_MGR $INSTALL_CMD "$Packagesix"
+    opkg update && opkg install "$Packagesix"
 fi
 
 if ! grep -qs "Package: $Packagerequests" "$STATUS"; then
-    echo "Installing $Packagerequests..."
-    $PKG_MGR update && $PKG_MGR $INSTALL_CMD "$Packagerequests"
+    echo "Need to install $Packagerequests"
+    if [ "$OSTYPE" = "DreamOs" ]; then
+        apt-get update && apt-get install "$Packagerequests" -y
+    else
+        opkg update && opkg install "$Packagerequests"
+    fi
 fi
 
-rm -rf "$TMPPATH" "$PLUGINPATH"
+[ -d "$TMPPATH" ] && rm -rf "$TMPPATH"
+[ -d "$PLUGINPATH" ] && rm -rf "$PLUGINPATH"
 mkdir -p "$TMPPATH"
 
-echo "Downloading $PYTHON version of SmartAddonspanel..."
-wget -q "$PLUGIN_URL/$PLUGIN_SUBDIR/SmartAddonspanel.tar.gz" -O "/tmp/SmartAddonspanel.tar.gz"
+cd /tmp
 
-if [ -f "/tmp/SmartAddonspanel.tar.gz" ]; then
-    tar -xzf "/tmp/SmartAddonspanel.tar.gz" -C "/tmp"
-    cp -r "/tmp/SmartAddonspanel/usr" "/"
-    sync
-
-    echo "#########################################################"
-    echo "#    Smart Addons panel INSTALLED SUCCESSFULLY          #"
-    echo "#                  Moded by Emil Nabil                  #"
-    echo "#########################################################"
+if [ "$PYTHON" = "PY3" ]; then
+    echo "Downloading Python 3 version of SmartAddonspanel..."
+    wget "$PLUGIN_URL/Py3/SmartAddonspanel.tar.gz" -O "/tmp/SmartAddonspanel.tar.gz"
 else
-    echo "Failed to download SmartAddonspanel.tar.gz!"
+    echo "Downloading Python 2 version of SmartAddonspanel..."
+    wget "$PLUGIN_URL/Py2/SmartAddonspanel.tar.gz" -O "/tmp/SmartAddonspanel.tar.gz"
+fi
+
+if [ $? -ne 0 ]; then
+    echo "Failed to download the plugin. Exiting."
     exit 1
 fi
 
-RESTART_CMD="systemctl restart enigma2"
-[ ! -d /usr/lib64 ] && RESTART_CMD="killall -9 enigma2"
+tar -xzf "/tmp/SmartAddonspanel.tar.gz"
+if [ $? -ne 0 ]; then
+    echo "Failed to extract the plugin. Exiting."
+    exit 1
+fi
 
-rm -rf "$TMPPATH" "/tmp/SmartAddonspanel.tar.gz"
+cp -r "/tmp/SmartAddonspanel/usr" "/"
+sync
+
+echo "#########################################################"
+echo "#    Smart Addons panel INSTALLED SUCCESSFULLY          #"
+echo "#                  Moded by Emil Nabil                  #"
+echo "#########################################################"
+
+if [ ! -d /usr/lib64 ]; then
+    RESTART_CMD="killall -9 enigma2"
+else
+    RESTART_CMD="systemctl restart enigma2"
+fi
+
+cd /tmp || exit 1
+rm -rf "$TMPPATH" /tmp/SmartAddonspanel.tar.gz > /dev/null 2>&1
 sync
 
 echo "#########################################################"
@@ -96,6 +95,4 @@ sleep 5
 $RESTART_CMD
 
 exit 0
-
-
 
