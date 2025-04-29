cb7348d478ea866fb6f105150ec4bbb74442424c
emil-nabil333 <elbrinselomda@yahoo.com>
2025-04-29 10:31:46 +0300

    Add files via upload

--- vhannibal-settings/vhannibal-settings.sh
+++ vhannibal-settings/vhannibal-settings.sh
@@ -22,7 +22,7 @@ fi
 
 cd /tmp || exit
 echo "Downloading the package..."
-curl -kL --max-time 60 "https://dreambox4u.com/emilnabil237/vhannibal-settings/vhannibal-settings-motor.tar.gz" -o vhannibal-settings-motor.tar.gz || { echo "Download failed"; exit 1; }
+curl -kL --max-time 60 "https://github.com/emilnabil/download-plugins/raw/refs/heads/main/vhannibal-settings/vhannibal-settings-motor.tar.gz" -o vhannibal-settings-motor.tar.gz || { echo "Download failed"; exit 1; }
 
 echo "Installing..."
 tar -xzf vhannibal-settings-motor.tar.gz -C / || { echo "Extraction failed"; exit 1; }
@@ -33,4 +33,5 @@ echo -e "\n>>>>>>>>>>>>>>>>>>> DONE <<<<<<<<<<<<<<<<<<<<<"
 sleep 2
 
 exit 0
+
 
