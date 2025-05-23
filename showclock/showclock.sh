4079b1c22d76f4d0fcdafcd0c4b4178492c18769
emil-nabil333 <elbrinselomda@yahoo.com>
2025-05-23 10:36:32 +0300

    Add files via upload

--- showclock/showclock.sh
+++ showclock/showclock.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #
+##setup command=wget https://raw.githubusercontent.com/emilnabil/download-plugins/refs/heads/main/showclock/showclock.sh -O - | /bin/sh
+
+########
 rm -rf /usr/lib/enigma2/python/Plugins/Extensions/ShowClock
 # Download and extract the package
 cd /tmp || exit
-curl -k -Lbk -m 55532 -m 555104 "https://dreambox4u.com/emilnabil237/skins/skins-aglare-fhd.tar.gz" -o /tmp/showclock.tar.gz
+curl -k -Lbk -m 55532 -m 555104 "https://github.com/emilnabil/download-plugins/raw/refs/heads/main/showclock/showclock.tar.gz" -o /tmp/showclock.tar.gz
 sleep 1
 echo "Installing ...."
 tar -xzf /tmp/showclock.tar.gz -C /
@@ -13,8 +16,5 @@ sleep 1
 rm -f /tmp/showclock.tar.gz
 sleep 2
 exit 0
-
-
-
 
 
