029c9a667026429372fc4019ec2efd8953ff6e7c
emil-nabil333 <elbrinselomda@yahoo.com>
2025-05-26 07:15:41 +0300

    Add files via upload

--- advancedscreenshot/advancedscreenshot.sh
+++ advancedscreenshot/advancedscreenshot.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
+##setup command=wget https://raw.githubusercontent.com/emilnabil/download-plugins/refs/heads/main/advancedscreenshot/advancedscreenshot.sh -O - | /bin/sh ###
+####################################
 echo "install plugin advancedscreenshot"
 cd /tmp
-curl  -k -Lbk -m 55532 -m 555104 "https://drive.google.com/uc?id=1GUJn8CUhv3KfmeqS-QJXvvCqy5Q79Pqz&export=download" > /tmp/enigma2-plugin-extensions-advancedscreenshot_1.2_all.ipk
+curl  -k -Lbk -m 55532 -m 555104 "https://github.com/emilnabil/download-plugins/raw/refs/heads/main/advancedscreenshot/enigma2-plugin-extensions-advancedscreenshot_all.ipk" > /tmp/enigma2-plugin-extensions-advancedscreenshot_all.ipk
 sleep 1
 echo "install plugin...."
 cd /tmp
-opkg install /tmp/enigma2-plugin-extensions-advancedscreenshot_1.2_all.ipk
+opkg install /tmp/enigma2-plugin-extensions-advancedscreenshot_all.ipk
 echo ""
 echo ""
 echo ""
@@ -15,6 +17,6 @@ echo ""
 echo ""
 echo ""
 sleep 1
-rm /tmp/enigma2-plugin-extensions-advancedscreenshot_1.2_all.ipk
+rm /tmp/enigma2-plugin-extensions-advancedscreenshot_all.ipk
 sleep 2
 exit
