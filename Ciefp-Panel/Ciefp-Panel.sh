c3d72eb6f5d200bfc161563fc9035be2fd263c4b
emil-nabil333 <elbrinselomda@yahoo.com>
2025-06-15 20:45:04 +0300

    Add files via upload

--- Ciefp-Panel/Ciefp-Panel.sh
+++ Ciefp-Panel/Ciefp-Panel.sh
@@ -4,58 +4,71 @@
 ######### Only This 2 lines to edit with new version ######
 version='1.1'
 changelog='\nFix little bugs\nUpdated Picons List'
+
 TMPPATH=/tmp/CiefpsettingsPanel
+
 if [ ! -d /usr/lib64 ]; then
-PLUGINPATH=/usr/lib/enigma2/python/Plugins/Extensions/CiefpsettingsPanel
+    PLUGINPATH=/usr/lib/enigma2/python/Plugins/Extensions/CiefpSettingsPanelModeEmil
 else
-PLUGINPATH=/usr/lib64/enigma2/python/Plugins/Extensions/CiefpsettingsPanel
+    PLUGINPATH=/usr/lib64/enigma2/python/Plugins/Extensions/CiefpSettingsPanelModeEmil
 fi
+
 if [ -f /var/lib/dpkg/status ]; then
-STATUS="/var/lib/dpkg/status"
-OSTYPE="DreamOs"
+    STATUS="/var/lib/dpkg/status"
+    OSTYPE="DreamOs"
 else
-STATUS="/var/lib/opkg/status"
-OSTYPE="Dream"
+    STATUS="/var/lib/opkg/status"
+    OSTYPE="Dream"
 fi
+
 if python --version 2>&1 | grep -q '^Python 3\.'; then
-echo "You have Python3 image"
-PYTHON="PY3"
-Packagesix="python3-six"
-Packagerequests="python3-requests"
+    echo "You have Python3 image"
+    PYTHON="PY3"
+    Packagesix="python3-six"
+    Packagerequests="python3-requests"
 else
-echo "You have Python2 image"
-PYTHON="PY2"
-Packagerequests="python-requests"
+    echo "You have Python2 image"
+    PYTHON="PY2"
+    Packagerequests="python-requests"
 fi
+
 if [ "$PYTHON" = "PY3" ] && ! grep -qs "Package: $Packagesix" "$STATUS"; then
-opkg update && opkg install python3-six
+    opkg update > /dev/null 2>&1 && opkg install $Packagesix -y > /dev/null 2>&1
 fi
+
 if ! grep -qs "Package: $Packagerequests" "$STATUS"; then
-echo "Need to install $Packagerequests"
-if [ "$OSTYPE" = "DreamOs" ]; then
-apt-get update && apt-get install "$Packagerequests" -y
-else
-opkg update && opkg install "$Packagerequests"
+    echo "Need to install $Packagerequests"
+    if [ "$OSTYPE" = "DreamOs" ]; then
+        apt-get update > /dev/null 2>&1 && apt-get install "$Packagerequests" -y > /dev/null 2>&1
+    else
+        opkg update > /dev/null 2>&1 && opkg install "$Packagerequests" -y > /dev/null 2>&1
+    fi
 fi
-fi
+
 [ -d "$TMPPATH" ] && rm -rf "$TMPPATH"
 [ -d "$PLUGINPATH" ] && rm -rf "$PLUGINPATH"
+
 mkdir -p "$TMPPATH"
 cd "$TMPPATH" || exit 1
+
 if [ "$OSTYPE" = "DreamOs" ]; then
-echo "# Your image is OE2.5/2.6 #"
+    echo "# Your image is OE2.5/2.6 #"
 else
-echo "# Your image is OE2.0 #"
+    echo "# Your image is OE2.0 #"
 fi
+
 PLUGIN_URL="https://github.com/emilnabil/download-plugins/raw/refs/heads/main/Ciefp-Panel"
+
 if [ "$PYTHON" = "PY3" ]; then
-wget "$PLUGIN_URL/py3/Ciefp-Panel.tar.gz"
+    wget -q "$PLUGIN_URL/py3/Ciefp-Panel.tar.gz"
 else
-wget "$PLUGIN_URL/py2/Ciefp-Panel.tar.gz"
+    wget -q "$PLUGIN_URL/py2/Ciefp-Panel.tar.gz"
 fi
+
 tar -xzf Ciefp-Panel.tar.gz
 cp -r "Ciefp-Panel/usr" "/"
 sync
+
 echo "#########################################################"
 echo "#    Ciefp-Panel INSTALLED SUCCESSFULLY          #"
 echo "#                  developed by ciefp                   #"
@@ -63,15 +76,17 @@ echo "#                   Big thanks Qu4k3                    #"
 echo "#                  .::ciefpsettings::.                  #"
 echo "#                  https://Sat-Club.EU                  #"
 echo "#########################################################"
+
 if [ ! -d /usr/lib64 ]; then
-PLUGINPATH=/usr/lib/enigma2/python/Plugins/Extensions/CiefpsettingsPanel
-RESTART_CMD="killall -9 enigma2"
+    PLUGINPATH=/usr/lib/enigma2/python/Plugins/Extensions/CiefpSettingsPanelModeEmil
+    RESTART_CMD="killall -9 enigma2"
 else
-PLUGINPATH=/usr/lib64/enigma2/python/Plugins/Extensions/CiefpsettingsPanel
-RESTART_CMD="systemctl restart enigma2"
+    PLUGINPATH=/usr/lib64/enigma2/python/Plugins/Extensions/CiefpSettingsPanelModeEmil
+    RESTART_CMD="systemctl restart enigma2"
 fi
-...
+
 sync
+
 echo "#########################################################"
 echo "#    Ciefp-Panel INSTALLED SUCCESSFULLY          #"
 echo "#                  developed by ciefp                   #"
@@ -81,9 +96,9 @@ echo "#                  https://Sat-Club.EU                  #"
 echo "#########################################################"
 echo "#           Your device will RESTART now                #"
 echo "#########################################################"
+
 sleep 5
 $RESTART_CMD
 exit 0
-
 
 
