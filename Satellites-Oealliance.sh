193d556cc06148e09ccc2c69c5e8e7a75b80d4a0
emil-nabil333 <elbrinselomda@yahoo.com>
2025-06-21 21:59:42 +0300

    Add files via upload

---
+++ Satellites-Oealliance.sh
@@ -0,0 +1,1 @@
+wget -O /etc/tuxbox/satellites.xml https://raw.githubusercontent.com/oe-alliance/oe-alliance-tuxbox-common/master/src/satellites.xml
---
+++ Satellites-OpenPli.sh
@@ -0,0 +1,1 @@
+wget -O /etc/tuxbox/satellites.xml "https://raw.githubusercontent.com/OpenPLi/tuxbox-xml/master/xml/satellites.xml
