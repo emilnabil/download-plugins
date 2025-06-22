68c037276926b486ae6c60484dcc6d4af9f3030d
emil-nabil333 <elbrinselomda@yahoo.com>
2025-06-22 06:29:52 +0300

    Add files via upload

--- Satellites-Oealliance.sh
+++ Satellites-Oealliance.sh
@@ -1,1 +1,25 @@
-wget -O /etc/tuxbox/satellites.xml https://raw.githubusercontent.com/oe-alliance/oe-alliance-tuxbox-common/master/src/satellites.xml
+#!/bin/bash
+
+echo "> Updating satellites.xml file, please wait..."
+sleep 2
+
+DEST="/etc/tuxbox/satellites.xml"
+URL="https://raw.githubusercontent.com/oe-alliance/oe-alliance-tuxbox-common/master/src/satellites.xml"
+
+#
+mkdir -p "$(dirname "$DEST")"
+
+#
+if wget --no-check-certificate -q -O "$DEST" "$URL"; then
+    echo "> satellites.xml updated successfully."
+    sleep 2
+    echo "> Restarting Enigma2 to apply changes..."
+    killall -9 enigma2
+else
+    echo "> Failed to download satellites.xml"
+    exit 1
+fi
+
+
+
+
--- Satellites-OpenPli.sh
+++ Satellites-OpenPli.sh
@@ -1,1 +1,22 @@
-wget -O /etc/tuxbox/satellites.xml "https://raw.githubusercontent.com/OpenPLi/tuxbox-xml/master/xml/satellites.xml
+#!/bin/bash
+
+echo "> Updating satellites.xml file, please wait..."
+sleep 2
+
+DEST="/etc/tuxbox/satellites.xml"
+URL="https://raw.githubusercontent.com/OpenPLi/tuxbox-xml/master/xml/satellites.xml"
+
+#
+mkdir -p "$(dirname "$DEST")"
+
+#
+if wget --no-check-certificate -q -O "$DEST" "$URL"; then
+    echo "> satellites.xml updated successfully."
+    sleep 2
+    echo "> Restarting Enigma2 to apply changes..."
+    killall -9 enigma2
+else
+    echo "> Failed to download satellites.xml"
+    exit 1
+fi
+
