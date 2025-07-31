cde8234714644c1b516ded21257c40a9e4b67995
emil-nabil333 <elbrinselomda@yahoo.com>
2025-07-31 08:44:46 +0300

    Add files via upload

---
+++ skins-for-dreambox/hdsuisse.one4all.fhd.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+## command=curl -kL https://github.com/emilnabil/download-plugins/raw/refs/heads/main/skins-for-dreambox/hdsuisse.one4all.fhd.sh | bash
+######################
+
+TMPFILE="/tmp/hdsuisse.one4all.fhd.tar.gz"
+URL="https://github.com/emilnabil/download-plugins/raw/refs/heads/main/skins-for-dreambox/hdsuisse.one4all.fhd.tar.gz"
+
+cd /tmp || {
+    echo "Failed to change directory to /tmp"
+    exit 1
+}
+
+echo "Downloading plugin package..."
+if ! curl -kL "$URL" -o "$TMPFILE"; then
+    echo "Download failed!"
+    exit 1
+fi
+
+echo "Installing package..."
+if ! tar -xzf "$TMPFILE" -C /; then
+    echo "Extraction failed!"
+    rm -f "$TMPFILE"
+    exit 1
+fi
+
+echo "Cleaning up..."
+rm -f "$TMPFILE"
+
+echo ""
+echo "Installation complete!"
+echo ">>>>>>>>>>>>>>>>> DONE <<<<<<<<<<<<<<<<<"
+sleep 2
+exit 0
+
+
+
