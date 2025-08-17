1e8dfbfe3eeb88fb2345783dbbababb91bea9099
emil-nabil333 <elbrinselomda@yahoo.com>
2025-08-17 11:54:03 +0300

    Add files via upload

---
+++ skins-for-dreambox/Brushedalu-HD.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+## command=curl -kL https://github.com/emilnabil/download-plugins/raw/refs/heads/main/skins-for-dreambox/Brushedalu-HD.sh | bash
+######################
+
+TMPFILE="/tmp/Brushedalu-HD.tar.gz"
+URL="https://github.com/emilnabil/download-plugins/raw/refs/heads/main/skins-for-dreambox/Brushedalu-HD.tar.gz"
+
+cd /tmp || {
+    echo "Failed to change directory to /tmp"
+    exit 1
+}
+
+echo "Downloading plugin package..."
+if ! curl -kL "$URL" -o "$TMPFILE"; then
+    echo "Download failed!"
+    exit 1
+fi
+
+echo "Installing package..."
+if ! tar -xzf "$TMPFILE" -C /; then
+    echo "Extraction failed!"
+    rm -f "$TMPFILE"
+    exit 1
+fi
+
+echo "Cleaning up..."
+rm -f "$TMPFILE"
+
+echo ""
+echo "Installation complete!"
+echo ">>>>>>>>>>>>>>>>> DONE <<<<<<<<<<<<<<<<<"
+sleep 2
+exit 0
+
+
+
+
