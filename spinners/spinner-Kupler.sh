0b16a184cc3ace347b65943c396cd3e22f6e6a2a
emil-nabil333 <elbrinselomda@yahoo.com>
2025-10-29 12:20:11 +0300

    Add files via upload

--- spinners/spinner-Kupler.sh
+++ spinners/spinner-Kupler.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 #
-###Command=curl -kLs https://github.com/emilnabil/download-plugins/raw/refs/heads/main/spinners/spinner-Kupler.sh|sh
+### Command=curl -kLs https://github.com/emilnabil/download-plugins/raw/refs/heads/main/spinners/spinner-Kupler.sh | sh
 #####################
+
 echo "Removing previous spinners ..."
 sleep 2
 
-    rm -rf /usr/share/enigma2/spinner/* > /dev/null 2>&1
-    
-    echo 'spinner removed.'
+rm -rf /usr/share/enigma2/spinner/* >/dev/null 2>&1
+echo "Spinner removed."
 
-# Download and extract the spinner
-cd /tmp || exit
-curl -k -Lbk -m 55532 -m 555104 "https://github.com/emilnabil/download-plugins/raw/refs/heads/main/spinners/spinner-Kupler.tar.gz" -o /tmp/spinner-Kupler.tar.gz
-sleep 1
-echo "Installing ...."
-tar -xzf /tmp/spinner-Kupler.tar.gz -C /
-echo ""
-echo ""
-sleep 1
-rm -f /tmp/spinner-Kupler.tar.gz
+# Download and extract the new spinner
+cd /tmp || exit 1
+
+echo "Downloading spinner..."
+curl -k -L -m 60 "https://github.com/emilnabil/download-plugins/raw/refs/heads/main/spinners/spinner-Kupler.tar.gz" -o /tmp/spinner-Kupler.tar.gz
+
+if [ -f /tmp/spinner-Kupler.tar.gz ]; then
+    echo "Installing..."
+    tar -xzf /tmp/spinner-Kupler.tar.gz -C /
+    echo ""
+    sleep 1
+    rm -f /tmp/spinner-Kupler.tar.gz
+    echo "Installation complete."
+else
+    echo "Download failed!"
+fi
+
 sleep 2
 exit 0
-
-
-
-
 
 
