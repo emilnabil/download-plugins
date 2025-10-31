911d0a3903f280fe2c7038fc1ae89f0125abf3b2
emil-nabil333 <elbrinselomda@yahoo.com>
2025-10-31 09:39:58 +0200

    Add files via upload

---
+++ spinners/spinner-Jack-Sparrow.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+### Command=curl -kLs https://github.com/emilnabil/download-plugins/raw/refs/heads/main/spinners/spinner-Jack-Sparrow.sh | sh
+#####################
+
+echo "Removing previous spinners ..."
+sleep 2
+
+rm -rf /usr/share/enigma2/spinner/* >/dev/null 2>&1
+rm -rf /usr/share/enigma2/skin_default/spinner/* >/dev/null 2>&1
+echo "Spinner removed."
+sleep 1
+
+# Download and extract the new spinner
+cd /tmp || { echo "Failed to access /tmp"; exit 1; }
+
+echo "Downloading spinner..."
+curl -k -L -m 60 "https://github.com/emilnabil/download-plugins/raw/refs/heads/main/spinners/spinner-Jack-Sparrow.tar.gz" -o spinner-Jack-Sparrow.tar.gz
+
+if [ -s spinner-Jack-Sparrow.tar.gz ]; then
+    echo "Installing..."
+    tar -xzf spinner-Jack-Sparrow.tar.gz -C / || { echo "Extraction failed!"; exit 1; }
+    sleep 1
+
+    if [ -d /usr/share/enigma2/spinner ]; then
+        cp -p /usr/share/enigma2/spinner/* /usr/share/enigma2/skin_default/spinner/ >/dev/null 2>&1
+    fi
+
+    rm -f spinner-Jack-Sparrow.tar.gz
+    echo "Installation complete."
+else
+    echo "Download failed!"
+    exit 1
+fi
+
+sleep 2
+echo "Restarting Enigma2..."
+killall -9 enigma2 >/dev/null 2>&1
+exit 0
+
+
+
+
+
+
