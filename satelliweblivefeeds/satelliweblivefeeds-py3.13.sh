7365341852bc47d72ce34f687d48f22487bcb77e
emil-nabil333 <elbrinselomda@yahoo.com>
2025-12-08 15:12:07 +0200

    Add files via upload

--- satelliweblivefeeds/satelliweblivefeeds-py3.13.sh
+++ satelliweblivefeeds/satelliweblivefeeds-py3.13.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ##Command=wget --no-check-certificate -O - https://github.com/emilnabil/download-plugins/raw/refs/heads/main/satelliweblivefeeds/satelliweblivefeeds-py3.13.sh -O - | /bin/sh
 ############################
-echo "Removing previous version of Skin..."
+echo "Removing previous version ..."
 sleep 2
 
 if [ -d /usr/lib/enigma2/python/Plugins/Extensions/Fatima ]; then
@@ -16,19 +16,20 @@ opkg install curl
 sleep 2
 
 cd /tmp || exit
-curl -k -L "https://github.com/emilnabil/download-plugins/raw/refs/heads/main/E2BissKeysEditor/E2BissKeysEditor.tar.gz" -o /tmp/E2BissKeysEditor.tar.gz
+curl -k -L "https://github.com/emilnabil/download-plugins/raw/refs/heads/main/satelliweblivefeeds/satelliweblivefeeds-py3.13.tar.gz" -o /tmp/satelliweblivefeeds-py3.13.tar.gz
 sleep 2
 
 echo "Installing ...."
-tar -xzf /tmp/E2BissKeysEditor.tar.gz -C /
+tar -xzf /tmp/satelliweblivefeeds-py3.13.tar.gz -C /
 echo ""
 echo ""
 
 sleep 1
-rm -f /tmp/E2BissKeysEditor.tar.gz
+rm -f /tmp/satelliweblivefeeds-py3.13.tar.gz
 echo " Uploaded By Emil Nabil"
 sleep 2
 exit 0
+
 
 
 
