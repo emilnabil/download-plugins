48d6d1418496817415ef0d5f3c2c0b9f167939d6
emil-nabil333 <elbrinselomda@yahoo.com>
2025-12-24 13:22:13 +0200

    Add files via upload

---
+++ AISubtitles/AISubtitles.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "Removing previous version ..."
+sleep 2
+
+if [ -d /usr/lib/enigma2/python/Plugins/Extensions/AISubtitles ]; then
+    rm -rf //usr/lib/enigma2/python/Plugins/Extensions/AISubtitles > /dev/null 2>&1
+    echo 'Package removed.'
+else
+    echo "You do not have previous version"
+fi
+
+echo ""
+opkg install curl
+sleep 2
+
+cd /tmp || exit
+curl -k -Lbk -m 55532 -m 555104 "https://dreambox4u.com/emilnabil237/skins/skins-aglare-fhd.tar.gz" -o /tmp/AISubtitles.tar.gz
+sleep 1
+echo "Installing ...."
+tar -xzf /tmp/AISubtitles.tar.gz -C /
+echo ""
+echo ""
+sleep 1
+rm -f /tmp/AISubtitles.tar.gz
+
+echo ">>>>>>>>>>Uploaded By Emil Nabil <<<<<<<<<<"
+sleep 2
+exit 0
+
+
+
+
+
