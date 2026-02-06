582633457580759095fe5daa379a534fcf495bb9
emil-nabil333 <elbrinselomda@yahoo.com>
2026-02-06 12:01:42 +0200

    Add files via upload

--- corvoboys-panel/corvoboys-panel.sh
+++ corvoboys-panel/corvoboys-panel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-##Command=wget https://dreambox4u.com/emilnabil237/skins/openatv/nacht_7.1.sh -O - | /bin/sh
+##Command=wget https://github.com/emilnabil/download-plugins/raw/refs/heads/main/corvoboys-panel/corvoboys-panel.sh -O - | /bin/sh
 ####################
 echo "Removing previous version..."
 sleep 2
@@ -18,7 +18,7 @@ sleep 2
 
 # Download and extract the package
 cd /tmp || exit
-curl -k -Lbk -m 55532 -m 555104 "https://dreambox4u.com/emilnabil237/skins/openatv/skins-nacht_7.1.tar.gz" -o /tmp/cbPanel.tar.gz
+curl -k -Lbk -m 55532 -m 555104 "https://github.com/emilnabil/download-plugins/raw/refs/heads/main/corvoboys-panel/cbPanel.tar.gz" -o /tmp/cbPanel.tar.gz
 sleep 1
 echo "Installing ...."
 tar -xzf /tmp/cbPanel.tar.gz -C /
@@ -28,6 +28,7 @@ sleep 1
 rm -f /tmp/cbPanel.tar.gz
 sleep 2
 exit 0
+
 
 
 
