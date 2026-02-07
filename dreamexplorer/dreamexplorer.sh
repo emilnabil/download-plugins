00126418ca2e2235d6c937bddb05967b6edcb9ac
emil-nabil333 <elbrinselomda@yahoo.com>
2026-02-07 14:40:58 +0200

    Add files via upload

--- dreamexplorer/dreamexplorer.sh
+++ dreamexplorer/dreamexplorer.sh
@@ -5,8 +5,8 @@ echo "Removing previous version..."
 sleep 2
 
 # Check if the directory exists before removing it
-if [ -d /usr/lib/enigma2/python/Plugins/Extensions/cbPanel ]; then
-    rm -rf /usr/lib/enigma2/python/Plugins/Extensions/cbPanel
+if [ -d /usr/lib/enigma2/python/Plugins/Extensions/DreamExplorer ]; then
+    rm -rf /usr/lib/enigma2/python/Plugins/Extensions/DreamExplorer
     echo 'Package removed.'
 else
     echo "You do not have previous version"
@@ -18,7 +18,7 @@ sleep 2
 
 # Download and extract the package
 cd /tmp || exit
-curl -k -Lbk -m 55532 -m 555104 "https://github.com/emilnabil/download-plugins/raw/refs/heads/main/corvoboys-panel/cbPanel.tar.gz" -o /tmp/dreamexplorer.tar.gz
+curl -k -Lbk -m 55532 -m 555104 "https://github.com/emilnabil/download-plugins/raw/refs/heads/main/dreamexplorer/dreamexplorer.tar.gz" -o /tmp/dreamexplorer.tar.gz
 sleep 1
 echo "Installing ...."
 tar -xzf /tmp/dreamexplorer.tar.gz -C /
@@ -28,16 +28,5 @@ sleep 1
 rm -f /tmp/dreamexplorer.tar.gz
 sleep 2
 exit 0
-
-
-
-
-
-
-
-
-
-
-
 
 
