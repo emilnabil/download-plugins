0f1ae3c2138e8b5e7e8971ad4f33141d3a2aa619
emil-nabil333 <elbrinselomda@yahoo.com>
2026-03-24 18:51:48 +0200

    Add files via upload

--- AutomaticVolumeAdjustment/AutomaticVolumeAdjustment.sh
+++ AutomaticVolumeAdjustment/AutomaticVolumeAdjustment.sh
@@ -4,15 +4,15 @@
 echo "install plugin"
 cd /tmp
 if command -v wget >/dev/null 2>&1; then
-    wget -q --no-check-certificate -O softwaremanager.tar.gz "https://github.com/emilnabil/download-plugins/raw/refs/heads/main/softwaremanager/softwaremanager.tar.gz"
+    wget -q --no-check-certificate -O AutomaticVolumeAdjustment.tar.gz "https://github.com/emilnabil/download-plugins/raw/refs/heads/main/AutomaticVolumeAdjustment/AutomaticVolumeAdjustment.tar.gz"
 else
-    curl -k -L -o softwaremanager.tar.gz "https://github.com/emilnabil/download-plugins/raw/refs/heads/main/softwaremanager/softwaremanager.tar.gz"
+    curl -k -L -o AutomaticVolumeAdjustment.tar.gz "https://github.com/emilnabil/download-plugins/raw/refs/heads/main/AutomaticVolumeAdjustment/AutomaticVolumeAdjustment.tar.gz"
 fi
 sleep 1
 echo "install plugin...."
 cd /tmp
-tar -xzf softwaremanager.tar.gz -C /
-sleep 1
-rm -f softwaremanager.tar.gz
+tar -xzf AutomaticVolumeAdjustment.tar.gz -C /
+sleep 2
+rm -f AutomaticVolumeAdjustment.tar.gz
 sleep 2
 exit
