a4cc93fafe3c6d2530dbd424d817b24b6df3e0c2
emil-nabil333 <elbrinselomda@yahoo.com>
2026-03-24 18:49:01 +0200

    Add files via upload

--- AutomaticVolumeAdjustment/AutomaticVolumeAdjustment.sh
+++ AutomaticVolumeAdjustment/AutomaticVolumeAdjustment.sh
@@ -1,1 +1,18 @@
-
+#!/bin/sh
+##command=wget -q "--no-check-certificate" https://github.com/emilnabil/download-plugins/raw/refs/heads/main/AutomaticVolumeAdjustment/AutomaticVolumeAdjustment.sh -O - | /bin/sh
+#########################################
+echo "install plugin"
+cd /tmp
+if command -v wget >/dev/null 2>&1; then
+    wget -q --no-check-certificate -O softwaremanager.tar.gz "https://github.com/emilnabil/download-plugins/raw/refs/heads/main/softwaremanager/softwaremanager.tar.gz"
+else
+    curl -k -L -o softwaremanager.tar.gz "https://github.com/emilnabil/download-plugins/raw/refs/heads/main/softwaremanager/softwaremanager.tar.gz"
+fi
+sleep 1
+echo "install plugin...."
+cd /tmp
+tar -xzf softwaremanager.tar.gz -C /
+sleep 1
+rm -f softwaremanager.tar.gz
+sleep 2
+exit
