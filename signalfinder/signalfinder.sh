0d55a9af317d3a1beacbcf6be4492f4fcf758e1b
emil-nabil333 <elbrinselomda@yahoo.com>
2026-03-25 10:25:11 +0200

    Add files via upload

--- signalfinder/signalfinder.sh
+++ signalfinder/signalfinder.sh
@@ -1,1 +1,18 @@
-
+#!/bin/sh
+##command=wget -q "--no-check-certificate" https://github.com/emilnabil/download-plugins/raw/refs/heads/main/signalfinder/signalfinder.sh -O - | /bin/sh
+#########################################
+echo "install plugin"
+cd /tmp
+if command -v wget >/dev/null 2>&1; then
+    wget -q --no-check-certificate -O Signalfinder.tar.gz "https://github.com/emilnabil/download-plugins/raw/refs/heads/main/signalfinder/Signalfinder.tar.gz"
+else
+    curl -k -L -o Signalfinder.tar.gz "https://github.com/emilnabil/download-plugins/raw/refs/heads/main/signalfinder/Signalfinder.tar.gz"
+fi
+sleep 1
+echo "install plugin...."
+cd /tmp
+tar -xzf Signalfinder.tar.gz -C /
+sleep 2
+rm -f Signalfinder.tar.gz
+sleep 2
+exit
