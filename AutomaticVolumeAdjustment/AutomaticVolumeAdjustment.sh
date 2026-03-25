c33e725e72efdc5fd458d96e0ec4ceee409af52a
emil-nabil333 <elbrinselomda@yahoo.com>
2026-03-25 16:48:14 +0200

    Add files via upload

--- AutomaticVolumeAdjustment/AutomaticVolumeAdjustment.sh
+++ AutomaticVolumeAdjustment/AutomaticVolumeAdjustment.sh
@@ -2,17 +2,37 @@
 ##command=wget -q "--no-check-certificate" https://github.com/emilnabil/download-plugins/raw/refs/heads/main/AutomaticVolumeAdjustment/AutomaticVolumeAdjustment.sh -O - | /bin/sh
 #########################################
 echo "install plugin"
+
 cd /tmp
-if command -v wget >/dev/null 2>&1; then
-    wget -q --no-check-certificate -O AutomaticVolumeAdjustment.tar.gz "https://github.com/emilnabil/download-plugins/raw/refs/heads/main/AutomaticVolumeAdjustment/AutomaticVolumeAdjustment.tar.gz"
+if [ -f /etc/openpli-version ] || [ -f /etc/openpli-release ] || grep -q "openpli" /etc/issue 2>/dev/null; then
+    IMAGE_TYPE="openpli"
+    echo "Detected OpenPLI image"
+    PLUGIN_URL="https://github.com/emilnabil/download-plugins/raw/refs/heads/main/AutomaticVolumeAdjustment/AutomaticVolumeAdjustment-pli.tar.gz"
 else
-    curl -k -L -o AutomaticVolumeAdjustment.tar.gz "https://github.com/emilnabil/download-plugins/raw/refs/heads/main/AutomaticVolumeAdjustment/AutomaticVolumeAdjustment.tar.gz"
+    IMAGE_TYPE="other"
+    echo "Detected other image"
+    PLUGIN_URL="https://github.com/emilnabil/download-plugins/raw/refs/heads/main/AutomaticVolumeAdjustment/AutomaticVolumeAdjustment.tar.gz"
 fi
-sleep 1
-echo "install plugin...."
-cd /tmp
-tar -xzf AutomaticVolumeAdjustment.tar.gz -C /
-sleep 2
-rm -f AutomaticVolumeAdjustment.tar.gz
+
+if command -v wget >/dev/null 2>&1; then
+    wget -q --no-check-certificate -O AutomaticVolumeAdjustment.tar.gz "$PLUGIN_URL"
+else
+    curl -k -L -o AutomaticVolumeAdjustment.tar.gz "$PLUGIN_URL"
+fi
+
+if [ $? -eq 0 ] && [ -f AutomaticVolumeAdjustment.tar.gz ]; then
+    echo "Download successful for $IMAGE_TYPE image"
+    sleep 1
+    echo "Installing plugin...."
+    cd /tmp
+    tar -xzf AutomaticVolumeAdjustment.tar.gz -C /
+    sleep 2
+    rm -f AutomaticVolumeAdjustment.tar.gz
+    echo "Plugin installed successfully"
+else
+    echo "Download failed!"
+    exit 1
+fi
+
 sleep 2
 exit
