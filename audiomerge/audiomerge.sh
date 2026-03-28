95ce28d8e0ba3459b76117bca22d120d381d081f
emil-nabil333 <elbrinselomda@yahoo.com>
2026-03-28 10:14:17 +0200

    Add files via upload

--- audiomerge/audiomerge.sh
+++ audiomerge/audiomerge.sh
@@ -4,15 +4,15 @@
 echo "install plugin"
 cd /tmp
 if command -v wget >/dev/null 2>&1; then
-    wget -q --no-check-certificate -O setpasswd.tar.gz "https://github.com/emilnabil/download-plugins/raw/refs/heads/main/setpasswd.tar.gz"
+    wget -q --no-check-certificate -O audiomerge.tar.gz "https://github.com/emilnabil/download-plugins/raw/refs/heads/main/audiomerge/audiomerge.tar.gz"
 else
-    curl -k -L -o setpasswd.tar.gz "https://github.com/emilnabil/download-plugins/raw/refs/heads/main/setpasswd.tar.gz"
+    curl -k -L -o audiomerge.tar.gz "https://github.com/emilnabil/download-plugins/raw/refs/heads/main/audiomerge/audiomerge.tar.gz"
 fi
 sleep 1
 echo "install plugin...."
 cd /tmp
-tar -xzf setpasswd.tar.gz -C /
+tar -xzf audiomerge.tar.gz -C /
 sleep 2
-rm -f setpasswd.tar.gz
+rm -f audiomerge.tar.gz
 sleep 2
 exit
