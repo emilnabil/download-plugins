95511c71d197b976c035a8c04bb4e208f79ac4bf
emil-nabil333 <elbrinselomda@yahoo.com>
2026-04-14 10:45:11 +0200

    Add files via upload

--- Skin-MX_Maestra_Black_weather/Skin-MX_Maestra_Black_weather.sh
+++ Skin-MX_Maestra_Black_weather/Skin-MX_Maestra_Black_weather.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ##command=wget -q "--no-check-certificate" https://github.com/emilnabil/download-plugins/raw/refs/heads/main/Skin-MX_Maestra_Black_weather/Skin-MX_Maestra_Black_weather.sh -O - | /bin/sh
-#######################
-PYTHON_VERSION=$(python -c"import platform; print(platform.python_version())")
+##################################
+PYTHON_VERSION=$(python -c "import platform; print('.'.join(platform.python_version().split('.')[:2]))")
 
 set -e
 
@@ -11,28 +11,47 @@ rm -rf /usr/share/enigma2/MX_Maestra_Black_weather
 echo "Installing plugin..."
 cd /tmp || { echo "Failed to enter /tmp"; exit 1; }
 
+case "$PYTHON_VERSION" in
+    3.12)
+        URL="https://github.com/emilnabil/download-plugins/raw/refs/heads/main/Skin-MX_Maestra_Black_weather/Skin-MX_Maestra_Black-weather_py3.12.tar.gz"
+        ;;
+    3.13)
+        URL="https://github.com/emilnabil/download-plugins/raw/refs/heads/main/Skin-MX_Maestra_Black_weather/Skin-MX_Maestra_Black-weather_py3.13.tar.gz"
+        ;;
+    3.14)
+        URL="https://github.com/emilnabil/download-plugins/raw/refs/heads/main/Skin-MX_Maestra_Black_weather/Skin-MX_Maestra_Black_weather_py3.14.tar.gz"
+        ;;
+    *)
+        echo "Unsupported Python version: $PYTHON_VERSION. Only 3.12, 3.13, 3.14 are supported."
+        exit 1
+        ;;
+esac
+
+echo "Detected Python $PYTHON_VERSION, downloading from: $URL"
+
+TAR_FILE="MX_Maestra_Black_weather.tar.gz"
 if command -v wget >/dev/null 2>&1; then
     echo "Downloading using wget..."
-    wget -q --no-check-certificate -O usr/share/enigma2/MX_Maestra_Black_weather.tar.gz "https://github.com/emilnabil/download-plugins/raw/refs/heads/main/usr/share/enigma2/MX_Maestra_Black_weather/usr/share/enigma2/MX_Maestra_Black_weather.tar.gz"
+    wget -q --no-check-certificate -O "$TAR_FILE" "$URL"
 elif command -v curl >/dev/null 2>&1; then
     echo "Downloading using curl..."
-    curl -k -L -o usr/share/enigma2/MX_Maestra_Black_weather.tar.gz "https://github.com/emilnabil/download-plugins/raw/refs/heads/main/usr/share/enigma2/MX_Maestra_Black_weather/usr/share/enigma2/MX_Maestra_Black_weather.gz"
+    curl -k -L -o "$TAR_FILE" "$URL"
 else
     echo "Error: neither wget nor curl is available to download the file."
     exit 1
 fi
 
-if [ ! -f usr/share/enigma2/MX_Maestra_Black_weather.tar.gz ]; then
+if [ ! -f "$TAR_FILE" ]; then
     echo "Error: failed to download the file."
     exit 1
 fi
 
 echo "Extracting..."
-tar -xzf usr/share/enigma2/MX_Maestra_Black_weather.tar.gz -C /
+tar -xzf "$TAR_FILE" -C /
 sleep 1
 
 echo "Cleaning temporary files..."
-rm -f usr/share/enigma2/MX_Maestra_Black_weather.tar.gz
+rm -f "$TAR_FILE"
 
 echo "Installation successful."
 exit 0
