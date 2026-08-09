6a57fd7a741a9862b95dbb0b4204edda100106cb
emil-nabil333 <elbrinselomda@yahoo.com>
2026-08-09 22:31:14 +0300

    Add files via upload

---
+++ PluginBrowser-Grid/pluginbrowser-grid.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd /usr/lib/enigma2/python/Screens || exit 1
+
+cp PluginBrowser.pyc PluginBrowser.pyc.bak
+
+wget -q https://github.com/emilnabil/download-plugins/raw/refs/heads/main/PluginBrowser-Grid/PluginBrowser.py -O PluginBrowser.py
+
+if [ $? -ne 0 ]; then
+    cp PluginBrowser.pyc.bak PluginBrowser.pyc
+    exit 1
+fi
+
+sleep 2
+killall -9 enigma2
+exit 0
