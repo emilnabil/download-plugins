31e7a3b05b6190ea4cafdd4d019f371a29c9bdec
emil-nabil333 <elbrinselomda@yahoo.com>
2026-08-13 18:42:14 +0300

    Add files via upload

--- menusort.sh
+++ menusort.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+##command=wget https://github.com/emilnabil/download-plugins/raw/refs/heads/main/menusort.sh -O - | /bin/sh
+######################
 wget -O /var/volatile/tmp/menusort_v1.0_all.ipk "https://raw.githubusercontent.com/emil237/plugins/main/menusort_v1.0_all.ipk"
 sleep 2
-opkg install --force-overwrite /tmp/*.ipk
-sleep 
+opkg install --force-overwrite /var/volatile/tmp/menusort_v1.0_all.ipk
+sleep 2
 rm -f /var/volatile/tmp/menusort_v1.0_all.ipk
-wait
 sleep 2
 exit 0
