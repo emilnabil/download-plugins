3394d2e465b72d319610e31d8232c2a13406dac1
emil-nabil333 <elbrinselomda@yahoo.com>
2026-08-18 19:56:43 +0300

    Add files via upload

--- emilbootlogos/emilbootlogos.sh
+++ emilbootlogos/emilbootlogos.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+##setup command=wget https://github.com/emilnabil/download-plugins/raw/refs/heads/main/emilbootlogos/emilbootlogos.sh -O - | /bin/sh
+
 echo "Removing previous version ..."
 sleep 2
 
-# Check if the directory exists before removing it
 if [ -d /usr/lib/enigma2/python/Plugins/Extensions/EmilBootLogos ]; then
     rm -rf /usr/lib/enigma2/python/Plugins/Extensions/EmilBootLogos > /dev/null 2>&1
     echo 'Package removed.'
@@ -15,9 +16,8 @@ echo ""
 opkg install curl
 sleep 2
 
-# Download and extract the package
 cd /tmp || exit
-curl -k -Lbk -m 55532 -m 555104 "https://dreambox4u.com/emilnabil237/skins/skins-aglare-fhd.tar.gz" -o /tmp/EmilBootLogos.tar.gz
+curl -k -Lbk -m 55532 -m 555104 "https://github.com/emilnabil/download-plugins/raw/refs/heads/main/emilbootlogos/EmilBootLogos.tar.gz" -o /tmp/EmilBootLogos.tar.gz
 sleep 1
 echo "Installing ...."
 tar -xzf /tmp/EmilBootLogos.tar.gz -C /
@@ -29,8 +29,4 @@ rm -f /tmp/EmilBootLogos.tar.gz
 echo ">>>>>>>>>>Uploaded by Emil Nabil <<<<<<<<<<"
 sleep 2
 exit 0
-
-
-
-
 
