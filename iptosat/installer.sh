3cfbd421242be4e425df7f16d00ea4c551215b38
emil-nabil333 <elbrinselomda@yahoo.com>
2026-08-21 22:15:08 +0300

    Add files via upload

---
+++ iptosat/installer.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+#wget -q "--no-check-certificate" https://raw.githubusercontent.com/emilnabil/iptosat/main/installer.sh -O - | /bin/sh
+
+version=1.8
+#############################################################
+TEMPATH=/tmp
+MY_IPK=enigma2-plugin-extensions-iptosat_1.8_all.ipk
+MY_DEB=enigma2-plugin-extensions-iptosat_1.8.deb
+MY_URL="https://raw.githubusercontent.com/emilnabil/iptosat/main"
+# remove old version #
+rm -rf /usr/lib/enigma2/python/Plugins/Extensions/IPtoSAT
+###########################################
+
+# Download and install plugin #
+
+echo " Download and install plugin"
+
+cd /tmp
+set -e
+     wget "$MY_URL/$MY_IPK"
+  wait
+     wget "$MY_URL/$MY_DEB"
+
+ if which dpkg > /dev/null 2>&1; then
+		dpkg -i --force-overwrite $MY_DEB; apt-get install -f -y
+	else
+		opkg install --force-reinstall $MY_IPK
+	fi
+echo "============================================================================================================================="
+set +e
+cd ..
+wait
+rm -f /tmp/$MY_IPK
+rm -f /tmp/$MY_DEB
+	if [ $? -eq 0 ]; then
+echo ">>>>  SUCCESSFULLY INSTALLED <<<<"
+fi
+		echo "********************************************************************************"
+echo "   UPLOADED BY  >>>>   EMIL_NABIL "   
+sleep 4;
+		echo ". >>>>         RESTARING     <<<<"
+echo "**********************************************************************************"
+wait
+killall -9 enigma2
+exit 0
