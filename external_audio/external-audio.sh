b99bc5d81fa92061862486109ce86794ecdd696a
emil-nabil333 <elbrinselomda@yahoo.com>
2026-08-24 10:41:50 +0300

    Add files via upload

---
+++ external_audio/external-audio.sh
@@ -0,0 +1,18 @@
+
+
+
+
+OPKG_DIR="/etc/opkg/"
+
+if ls "$OPKG_DIR" | grep -q "cortexa15hf-neon-vfpv4"; then
+    echo "Detected CPU architecture: Cortex-A15hf-neon-vfpv4"
+elif ls "$OPKG_DIR" | grep -q "cortexa9hf-neon"; then
+    echo "Detected CPU architecture: Cortex-A9hf-neon"
+elif ls "$OPKG_DIR" | grep -q "cortexa7hf-vfp"; then
+    echo "Detected CPU architecture: Cortex-A7hf-vfp"
+elif ls "$OPKG_DIR" | grep -q "armv7ahf-neon"; then
+    echo "Detected CPU architecture: ARMv7-Ahf-neon"
+else
+    echo "Unknown CPU architecture"
+fi
+
